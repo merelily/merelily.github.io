df7a369a7d975b5817f3a9a80fb1cb053f20dc12
Meredith <meredithleecase@gmail.com>
2016-03-29 21:14:50 -0500

    class stuff

--- ACA_Spring_2016/Week_8/Exercise_2/js/animation-scroll.js
+++ ACA_Spring_2016/Week_8/Exercise_2/js/animation-scroll.js
@@ -11,4 +11,24 @@ $(document).ready(function () {
         console.log(verticalScroll)
 
     });
+    
+    //four items fx//
+    
+        $(window).scroll(function() {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if(verticalScroll >= 610) {
+
+            $('#my_1').addClass('animated fadeInDown');
+            $('#my_1').removeClass ('hide_me');
+            $('#my_2').addClass('animated fadeInUp');
+            $('#my_2').removeClass ('hide_me');
+            $('#my_3').addClass('animated fadeInLeft');
+            $('#my_3').removeClass ('hide_me');
+            $('#my_4').addClass('animated fadeInRight');
+            $('#my_4').removeClass ('hide_me');
+        }
+            
+    });
 });
