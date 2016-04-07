eaca72b4929ac2564b5fb809149293ce8252361e
Meredith <meredithleecase@gmail.com>
2016-04-07 18:37:58 -0500

    animation working?

--- ACA_Spring_2016/Final_Project/js/animation-scroll.js
+++ ACA_Spring_2016/Final_Project/js/animation-scroll.js
@@ -1,15 +1,33 @@
 $(document).ready(function () {
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
         var verticalScroll = $(this).scrollTop();
 
-        if(verticalScroll >= 1) {
+        if (verticalScroll >= 1) {
 
             $('#my_down').addClass('animated bounce');
         }
 
     });
-   
-});
 
+    $(window).on('load', function () {
+        console.log($(window).scrollTop());
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 980) {
+
+            console.log("vertical scroll > 980");
+            
+            $('.my_circle').addClass('animated fadeInLeft');
+        }
+
+    });
+
+
+});
