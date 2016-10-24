29614abaf832bdecd2e6b38751639345809b70c6
Meredith <meredithleecase@gmail.com>
2016-10-24 16:25:38 -0500

    let’s see if this works

--- ACA_Spring_2016/Week_7/Exercise_2/js/modal.js
+++
@@ -1,7 +0,0 @@
-$('.thumbnail').click(function(){
-  	$('.modal-body').empty();
-  	var title = $(this).parent('a').attr("title");
-  	$('.modal-title').html(title);
-  	$($(this).parents('div').html()).appendTo('.modal-body');
-  	$('#myModal').modal({show:true});
-});
--- js/animation-scroll.js
+++ js/animation-scroll.js
@@ -1,13 +1,33 @@
 $(document).ready(function () {
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
 
         var verticalScroll = $(this).scrollTop();
 
-        if(verticalScroll >= x) {
+        if (verticalScroll >= 1) {
 
-            $('#element').addClass('effect');
+            $('#my_down').addClass('animated bounce');
         }
 
     });
+
+    $(window).on('load', function () {
+        console.log($(window).scrollTop());
+
+    });
+
+    $(window).scroll(function () {
+
+        var verticalScroll = $(this).scrollTop();
+
+        if (verticalScroll >= 980) {
+
+            console.log("vertical scroll > 980");
+            
+            $('.my_circle').addClass('animated fadeInLeft');
+        }
+
+    });
+
+
 });
